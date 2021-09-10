8a3ce204969cdc297fdcf184250fa379b799aaa6
Vadim Litvinov <vadlit@vadlit.ru>
2021-09-10 16:46:19 +0300

    fix

---
+++ install.sh
@@ -0,0 +1,4 @@
+(cd gateway; ./install.sh)
+(cd routes; ./install.sh)
+(cd sessions; ./install.sh)
+(cd users; ./install.sh)
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+./gateway/uninstall.sh
+./routes/uninstall.sh
+./sessions/uninstall.sh
+./users/uninstall.sh
